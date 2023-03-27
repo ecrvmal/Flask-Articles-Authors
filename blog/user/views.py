f365e701991288f50f853dfddfd5fc8dab739bed
Vladimir <ecrvmal@yandex.ru>
2023-03-27 10:05:05 -0700

    Lesson2_Jinja

--- blog/user/views.py
+++ blog/user/views.py
@@ -1,14 +1,20 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 user = Blueprint('user', __name__, url_prefix='/users', static_folder='../static')
 # the Blueprint will be registered in app.py
 # when request  localhost/users   > goes to the Bluepront (url_prefix param)
 # when request  localhost/users   > goes to function under @user.route('/')
 
+USERS = ['Alice', 'John', 'Mike']
+
 
 @user.route('/')
 def user_list():
-    return 'Hello_user!'
+    return render_template(
+        'users/list.html',
+        users=USERS,
+    )
+
 
 
 @user.route('/<pk>')
