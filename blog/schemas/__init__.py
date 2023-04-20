5e101a52f43d50a3bd2377df4c272c5bf6815aee
Vladimir <ecrvmal@yandex.ru>
2023-04-20 10:54:28 -0700

    Lesson9 Completed

--- blog/schemas/__init__.py
+++ blog/schemas/__init__.py
@@ -1,13 +1,12 @@
-import blog.schemas
 from blog.schemas.tag import TagSchema
-# from blog.schemas.user import UserSchema
-# from blog.schemas.article import ArticleSchema
-# from blog.schemas.author import AuthorSchema
+from blog.schemas.user import UserSchema
+from blog.schemas.article import ArticleSchema
+from blog.schemas.author import AuthorSchema
 
 
 __all__ = [
     'TagSchema',
-    # 'UserSchema',
-    # 'ArticleSchema',
-    # 'AuthorSchema',
+    'UserSchema',
+    'ArticleSchema',
+    'AuthorSchema',
 ]
