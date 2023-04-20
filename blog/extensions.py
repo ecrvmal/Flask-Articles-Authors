38fc1e3cacbc87ba783a47276ea3a4ca05622071
Vladimir <ecrvmal@yandex.ru>
2023-04-20 01:18:55 -0700

    Lesson9 No Table Tag

--- blog/extensions.py
+++ blog/extensions.py
@@ -1,9 +1,13 @@
+from flask_combo_jsonapi import Api
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import CSRFProtect
 
+
 login_manager = LoginManager()
 db = SQLAlchemy()
 migrate = Migrate()
 csrf = CSRFProtect()
+
+# api = Api()
